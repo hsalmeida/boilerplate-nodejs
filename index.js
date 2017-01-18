cf7c8222b8c1d234d54238d89b16d5d752315cd5
hsalmeida <hsalmeida@gmail.com>
2017-01-18 16:48:38 -0200

    firebase modules

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var firebase = require("firebase");
 var app = express();
+
+var config = {
+    apiKey: "AIzaSyDaGeeXHAy8prgu3cs1AAqyL3VYWPsHyJo",
+    authDomain: "smartdelivery-154417.firebaseapp.com",
+    databaseURL: "https://smartdelivery-154417.firebaseio.com",
+    storageBucket: "smartdelivery-154417.appspot.com",
+    messagingSenderId: "121932600601"
+};
+firebase.initializeApp(config);
 
 app.use(morgan('combined'));
 
